b6014e22dca5cef297a63c36d75d4400d18b55aa
R3ndrex <yacholohchtoli@gmail.com>
2024-11-03 17:58:20 +0200

    Add imports, eventlisteners for each nav button

--- src/script.js
+++ src/script.js
@@ -1,3 +1,23 @@
 import "./style.css";
+import CreatePage from "./utils/createPage.js";
+import MainPage from "./utils/main-page.js";
 
-alert(1);
+const buttonMenu = document.querySelector(".menu");
+const buttonAbout = document.querySelector(".about");
+const buttonHome = document.querySelector(".home");
+
+const content = document.querySelector("#content");
+const page = new CreatePage(content);
+const main = new MainPage(page);
+main.init();
+
+[buttonAbout, buttonMenu, buttonHome].forEach((element) => {
+    element.addEventListener("click", () => {
+        page.clearPage();
+    });
+});
+
+buttonHome.addEventListener("click", () => {
+    const main = new MainPage(page);
+    main.init();
+});
