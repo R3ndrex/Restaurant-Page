5eecd7944f5a6e815334c27f036bbefd2e756dc7
R3ndrex <yacholohchtoli@gmail.com>
2024-11-05 13:58:56 +0200

    Add basic class

---
+++ src/utils/menu-page.js
@@ -0,0 +1,8 @@
+class CreateMenuPage {
+    constructor(createPageObject) {
+        this.main = createPageObject;
+        this.article = this.main.createElement("article");
+    }
+}
+
+export default CreateMenuPage;
