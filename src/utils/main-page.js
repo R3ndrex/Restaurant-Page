8a221230e6f737ed4db4b1a84b8be86d954d055f
R3ndrex <yacholohchtoli@gmail.com>
2024-11-06 21:39:42 +0200

    Convert texts to lists, convert weekdays to better object,use forin for weekdays

--- src/utils/main-page.js
+++ src/utils/main-page.js
@@ -1,25 +1,19 @@
-const HeaderText = {
-    0: "Our Pizzeria",
-    1: "Our Menu",
-    2: "Time",
-    3: "Order Online",
-};
-
-const pText = {
-    0: `Whether you’re visiting us for a quick lunch, a family gathering, or a late-night craving, our team is here to make you feel at home with a slice that’s more than just pizza—it’s part of our family. Come in, relax, and let us share a piece of our story with you—one delicious bite at a time.`,
-    1: `Explore our menu and discover a variety of pizzas crafted for every palate. From the simplicity of a Neapolitan Margherita with fresh mozzarella, basil, and tomato sauce, to bold and creative options like our Truffle & Mushroom pizza, each dish is made to order and cooked to perfection. In addition to pizzas, our menu features fresh salads, Italian-inspired appetizers, and delicious desserts to complete your meal.`,
-    2: `Come savor our pizzas during our open hours – freshly made, just for you!`,
-    3: `Enjoy the taste of Our Pizza wherever you are! We offer easy online ordering for those nights when you want the comfort of our pizza at home, as well as take-out and delivery options.`,
-};
+const HeaderText = ["Our Pizzeria", "Our Menu", "Time", "Order Online"];
+const pText = [
+    `Whether you’re visiting us for a quick lunch, a family gathering, or a late-night craving, our team is here to make you feel at home with a slice that’s more than just pizza—it’s part of our family. Come in, relax, and let us share a piece of our story with you—one delicious bite at a time.`,
+    `Explore our menu and discover a variety of pizzas crafted for every palate. From the simplicity of a Neapolitan Margherita with fresh mozzarella, basil, and tomato sauce, to bold and creative options like our Truffle & Mushroom pizza, each dish is made to order and cooked to perfection. In addition to pizzas, our menu features fresh salads, Italian-inspired appetizers, and delicious desserts to complete your meal.`,
+    `Come savor our pizzas during our open hours – freshly made, just for you!`,
+    `Enjoy the taste of Our Pizza wherever you are! We offer easy online ordering for those nights when you want the comfort of our pizza at home, as well as take-out and delivery options.`,
+];
 
 const weekDays = {
-    0: [`Monday:`, `10:00-20:00`],
-    1: [`Tuesday:`, `10:00-20:00`],
-    2: [`Wednesday:`, `10:00-20:00`],
-    3: [`Thursday:`, `10:00-20:00`],
-    4: [`Friday:`, `10:00-20:00`],
-    5: [`Saturday:`, `10:00-18:00`],
-    6: [`Sunday:`, `10:00-18:00`],
+    Monday: `10:00-20:00`,
+    Tuesday: `10:00-20:00`,
+    Wednesday: `10:00-20:00`,
+    Thursday: `10:00-20:00`,
+    Friday: `10:00-20:00`,
+    Saturday: `10:00-18:00`,
+    Sunday: `10:00-18:00`,
 };
 
 class CreateMain {
@@ -30,9 +24,11 @@ class CreateMain {
     }
     CreateSchedule(days, parent) {
         const ul = this.main.createElement("ul", parent);
-        for (let j = 0; j < days; j++) {
-            const li = this.main.createElement("li", ul);
-            li.innerHTML = `<span>${weekDays[j][0]}</span> <span>${weekDays[j][1]}</span>`;
+        for (const day in days) {
+            if (Object.prototype.hasOwnProperty.call(days, day)) {
+                const li = this.main.createElement("li", ul);
+                li.innerHTML = `<span>${day}</span> <span>${days[day]}</span>`;
+            }
         }
     }
     init() {
@@ -40,9 +36,8 @@ class CreateMain {
             const section = this.main.createElement("section", "article");
             this.main.createElement("h2", section, HeaderText[i]);
             this.main.createElement("p", section, pText[i]);
-
             if (HeaderText[i] === "Time") {
-                this.CreateSchedule(7, section);
+                this.CreateSchedule(weekDays, section);
             }
             if (HeaderText[i] === "Order Online") {
                 this.main.createElement("button", section, "Order Now");
