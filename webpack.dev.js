0bd96f5c71e1ca9b816fa0d0e8eb5c4f66592ee8
R3ndrex <yacholohchtoli@gmail.com>
2024-11-08 22:25:43 +0200

    Add build prod script, change prod and dev into separate files with common file

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,33 +1,10 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const common = require("./webpack.common.js");
+const { default: merge } = require("webpack-merge");
 
-module.exports = {
+module.exports = merge(common, {
     mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
     },
-    plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html/,
-                use: "html-loader",
-            },
-            {
-                test: /\.(png|svg|jpe?g|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-};
+});
